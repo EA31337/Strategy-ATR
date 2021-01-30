a7a74850424936182fdb61096dc39f24eedafc8a
kenorb <kenorb@users.noreply.github.com>
2021-01-30 00:44:02 +0000

    Optimize SignalOpen params (pf=0.00; dd=0.00; tt=0)

--- config/EURUSD_M15.h
+++ config/EURUSD_M15.h
@@ -1,11 +1,14 @@
-/*
+/**
  * @file
  * Defines default strategy parameter values for the given timeframe.
  */
 
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_ATR_Params_M15 : ATRParams {
-  Indi_ATR_Params_M15() : ATRParams(indi_atr_defaults, PERIOD_M15) { shift = 0; }
+  Indi_ATR_Params_M15() : ATRParams(indi_atr_defaults, PERIOD_M15) {
+    period = 0;
+    shift = 0;
+  }
 } indi_atr_m15;
 
 // Defines strategy's parameter values for the given pair symbol and timeframe.
@@ -13,14 +16,14 @@ struct Stg_ATR_Params_M15 : StgParams {
   // Struct constructor.
   Stg_ATR_Params_M15() : StgParams(stg_atr_defaults) {
     lot_size = 0;
-    signal_open_method = 0;
-    signal_open_filter = 1;
-    signal_open_level = (float)10;
-    signal_open_boost = 0;
+    signal_open_method = -4;
+    signal_open_filter = 0;
+    signal_open_level = (float)0.0;
+    signal_open_boost = 1;
     signal_close_method = 0;
-    signal_close_level = (float)0;
+    signal_close_level = (float)0.0;
     price_stop_method = 1;
-    price_stop_level = (float)2;
+    price_stop_level = (float)0.0;
     tick_filter_method = 1;
     max_spread = 0;
   }
